806d54549b98888254f3d01b398ea4a7c51cc2cc
Opeyemi Oyekunle <opyzyle1960@gmail.com>
2019-04-25 09:08:19 +0100

    dvhjd

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.delete('/deleteUser', (req, res)=>{
 })
 
 if(process.env.NODE_ENV === 'production'){
-	app.use(express.static('buid/student-portal'))
+	app.use(express.static('student-portal/build'))
   app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname, 'student-portal', 'build', 'index.html'))
   })
